536ece7a525487798d2b0330a0d4dcf858b80532
Camilo Valdes <bolivar014@gmail.com>
2022-09-23 13:45:25 -0500

    creando evento para actualización de usuario

--- WebApi_TEST/Data/UsuarioData.cs
+++ WebApi_TEST/Data/UsuarioData.cs
@@ -33,5 +33,31 @@ namespace WebApi_TEST.Data
                 }
             }
         }
+
+        public static bool Modificar(Usuario objUsuario)
+        {
+            using (SqlConnection objConexion = new SqlConnection(Conexion.conexionDB))
+            {
+                SqlCommand cmd = new SqlCommand("usp_modificar", objConexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@documentoidentidad", objUsuario.DocumentoIdentidad);
+                cmd.Parameters.AddWithValue("@nombres", objUsuario.Nombres);
+                cmd.Parameters.AddWithValue("@telefono", objUsuario.Telefono);
+                cmd.Parameters.AddWithValue("@correo", objUsuario.Correo);
+                cmd.Parameters.AddWithValue("@ciudad", objUsuario.Ciudad);
+
+                // Ejecutamos evento
+                try
+                {
+                    objConexion.Open();
+                    cmd.ExecuteNonQuery();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
     }
 }
