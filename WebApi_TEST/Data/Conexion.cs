15afae760162807dec5602ccb010c18c57e096e3
Camilo Valdes <bolivar014@gmail.com>
2022-09-23 13:38:20 -0500

    creando conexión a db - generando modelo usuario

---
+++ WebApi_TEST/Data/Conexion.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApi_TEST.Data
+{
+    public class Conexion
+    {
+        // Cadena de conexión DB.
+        public static string conexionDB = "Data Source=.; Initial Catalog=DBPRUEBAS; Integrated Security=True;";
+    }
+}
---
+++ WebApi_TEST/Data/UsuarioData.cs
@@ -0,0 +1,37 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using WebApi_TEST.Models;
+
+namespace WebApi_TEST.Data
+{
+    public class UsuarioData
+    {
+        // Evento para registrar USUARIOS en base de datos
+        public static bool Registrar(Usuario objUsuario)
+        {
+            // Creamos conexión en DB desde data.Conexion
+            using(SqlConnection objConexion = new SqlConnection(Conexion.conexionDB))
+            {
+                // Ejecutamos SP usp_registrar y enviamos sus respectivos argumentos
+                SqlCommand cmd = new SqlCommand("usp_registrar", objConexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@documentoidentidad", objUsuario.DocumentoIdentidad);
+                cmd.Parameters.AddWithValue("@nombres", objUsuario.Nombres);
+                cmd.Parameters.AddWithValue("@telefono", objUsuario.Telefono);
+                cmd.Parameters.AddWithValue("@correo", objUsuario.Correo);
+                cmd.Parameters.AddWithValue("@ciudad", objUsuario.Ciudad);
+
+                // Ejecutamos evento
+                try
+                {
+                    objConexion.Open();
+                    cmd.ExecuteNonQuery();
+                    return true;
+                } catch ( Exception ex )
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
